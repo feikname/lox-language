aec9d1c1503797cdc801e85e3040d630b99a6096
Flavio Monteiro <flaviomonteiro2013@gmail.com>
2024-11-15 02:15:54 -0300

    mais do scanner

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -9,7 +9,25 @@ class Scanner {
 	private final String source;
 	private final List<Token> tokens = new ArrayList<>();
 
+	private int start = 0; // points to the first character in the lexeme being scanned
+	private int current = 0; // points at the character currently being considered
+	private int line = 1;
+
 	Scanner(String source) {
 		this.source = source;
+	}
+
+	List<Token> scanTokens() {
+		while (!isAtEnd()) {
+			// We are at the beginning of the next lexeme.
+			start = current;
+			scanToken();
+		}
+		tokens.add(new Token(EOF, "", null, line));
+		return tokens;
+	}
+
+	private boolean isAtEnd() {
+		return current >= source.length();
 	}
 }
