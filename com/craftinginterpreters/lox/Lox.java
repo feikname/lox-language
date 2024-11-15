14f509a638a18c2fa3a6c518c877a16005ef208c
Flavio Monteiro <flaviomonteiro2013@gmail.com>
2024-11-15 01:48:43 -0300

    implementar o runPrompt

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -1,6 +1,8 @@
 package com.craftinginterpreters.lox;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -20,5 +22,17 @@ public class Lox {
 	private static void runFile(String path) throws IOException {
 		byte[] bytes = Files.readAllBytes(Paths.get(path));
 		run(new String(bytes, Charset.defaultCharset()));
+	}
+
+	private static void runPrompt() throws IOException {
+		InputStreamReader input = new InputStreamReader(System.in);
+		BufferedReader reader = new BufferedReader(input);
+		for (;;) {
+			System.out.print("> ");
+			String line = reader.readLine();
+			if (line == null)
+				break;
+			run(line);
+		}
 	}
 }
